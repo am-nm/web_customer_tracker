2638243246712ee1be22783171508bf01ae06b7e
Ashish <ashish_mohanty10@yahoo.com>
2022-06-20 21:11:20 +0530

    added post method to save data to database

--- src/com/love2code/springdemo/controller/CustomerController.java
+++ src/com/love2code/springdemo/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.love2code.springdemo.entity.Customer;
@@ -30,4 +32,34 @@ public class CustomerController {
 		
 		return "list-customers";
 	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		
+		// create model attribute to bind form data
+		Customer theCustomer = new Customer();
+		
+		model.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		
+		// save the customer using service
+		customerService.saveCustomer(theCustomer);
+		
+		return "redirect:/customer/list";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
