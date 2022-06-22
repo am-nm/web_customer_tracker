cdfb3c257c63cc508cac47b4df195f68f11ee8c0
Ashish <ashish_mohanty10@yahoo.com>
2022-06-22 20:42:45 +0530

    added a form update method to handle the update request

--- src/com/love2code/springdemo/controller/CustomerController.java
+++ src/com/love2code/springdemo/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.love2code.springdemo.entity.Customer;
 import com.love2code.springdemo.service.CustomerService;
@@ -51,5 +52,18 @@ public class CustomerController {
 		customerService.saveCustomer(theCustomer);
 		
 		return "redirect:/customer/list";
-	}	
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int id, Model model) {
+		
+		// get customer from the customer service
+		Customer theCustomer = customerService.getCustomer(id);
+		
+		// set customer as model attribute to pre-populate the form
+		model.addAttribute("customer", theCustomer);
+		
+		// send over to the form
+		return "customer-form";
+	}
 }
