0dfe3920d85f3479b591778848ce2bd263bf9525
Ashish <ashish_mohanty10@yahoo.com>
2022-06-24 14:58:26 +0530

    update controller to read sort field

--- src/com/love2code/springdemo/controller/CustomerController.java
+++ src/com/love2code/springdemo/controller/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.love2code.springdemo.entity.Customer;
 import com.love2code.springdemo.service.CustomerService;
+import com.love2code.springdemo.util.SortUtils;
 
 @Controller
 @RequestMapping("/customer")
@@ -23,10 +24,23 @@ public class CustomerController {
 	private CustomerService customerService;
 
 	@GetMapping("/list")
-	public String listCustomers(Model model) {
+	public String listCustomers(Model model, @RequestParam(required=false) String sort) {
 		
 		// get customers from the customer service
-		List<Customer> theCustomers = customerService.getCustomers();
+		List<Customer> theCustomers = null;
+		
+		// check for sort field
+		if(sort != null) {
+			
+			int sortField = Integer.parseInt(sort);
+			
+			theCustomers = customerService.getCustomers(sortField);
+			
+		} else {
+			
+			// no sort field provided. default to sorting by last name
+			theCustomers = customerService.getCustomers(SortUtils.LAST_NAME);
+		}
 		
 		// add the customers to the model
 		model.addAttribute("customers", theCustomers);
@@ -87,5 +101,5 @@ public class CustomerController {
 		
 		return "list-customers";
 		
-	}
+	}		
 }
